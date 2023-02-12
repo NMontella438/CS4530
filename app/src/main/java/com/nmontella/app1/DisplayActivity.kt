d986ed3e61bfd1387d1d00731c773ae0f3f8f5ab
Nick <89880711+NMontella438@users.noreply.github.com>
2023-02-12 15:45:47 -0700

    Added second activity and all information gathering works

---
+++ app/src/main/java/com/nmontella/app1/DisplayActivity.kt
@@ -0,0 +1,11 @@
+package com.nmontella.app1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DisplayActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_display)
+    }
+}
--- app/src/main/java/com/nmontella/app1/MainActivity.kt
+++ app/src/main/java/com/nmontella/app1/MainActivity.kt
@@ -1,12 +1,15 @@
 package com.nmontella.app1
 
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.graphics.Bitmap
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.text.Editable
+import android.provider.MediaStore
 import android.view.View
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
+import android.widget.*
+import androidx.activity.result.contract.ActivityResultContracts
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
     //Data Variables
@@ -14,19 +17,20 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
     private var firstName: String? = null
     private var midName: String? = null
     private var lastName: String? = null
+    private var nameSubmit: Boolean? = false
 
     //UI Variables
     private var buttonSubmit: Button? = null
     private var buttonClear: Button? = null
+    private var buttonPic: Button? = null
     private var etFirstName: EditText? = null
     private var etMidName: EditText? = null
     private var etLastName: EditText? = null
-    private var tvDisplay: TextView? = null
+    private var picView: ImageView? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        tvDisplay = findViewById(R.id.tv_display)
         etFirstName = findViewById(R.id.et_firstName)
         etMidName = findViewById(R.id.et_middleName)
         etLastName = findViewById(R.id.et_lastName)
@@ -35,9 +39,14 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         buttonSubmit!!.setOnClickListener(this)
         buttonClear = findViewById(R.id.button_clear)
         buttonClear!!.setOnClickListener(this)
+        buttonPic = findViewById(R.id.button_picture)
+        buttonPic!!.setOnClickListener(this)
 
     }
 
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+    }
     override fun onClick(view: View) {
         when(view.id){
             R.id.button_clear -> {
@@ -46,11 +55,40 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
                 etLastName!!.setText("")
             }
             R.id.button_submit -> {
+
                 firstName = etFirstName!!.text.toString()
                 midName = etMidName!!.text.toString()
                 lastName = etLastName!!.text.toString()
+
+                if(firstName.isNullOrBlank() || lastName.isNullOrBlank()){
+                    Toast.makeText(this@MainActivity, "Enter a First and Last Name", Toast.LENGTH_SHORT).show()
+                }
+
                 fullName = firstName.plus(" ").plus(midName).plus(" ").plus(lastName)
-                tvDisplay!!.text = fullName
+                nameSubmit = true
+            }
+            R.id.button_picture -> {
+                val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+                try{
+                    cameraActivity.launch(cameraIntent)
+                }catch(ex: ActivityNotFoundException){
+                    //Do error handling here
+                }
+            }
+        }
+    }
+    private val cameraActivity = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){result ->
+        if(result.resultCode == RESULT_OK) {
+            picView = findViewById<View>(R.id.pictureView) as ImageView
+            //val extras = result.data!!.extras
+            //val thumbnailImage = extras!!["data"] as Bitmap?
+
+            if (Build.VERSION.SDK_INT >= 33) {
+                val thumbnailImage = result.data!!.getParcelableExtra("data", Bitmap::class.java)
+                picView!!.setImageBitmap(thumbnailImage)
+            } else {
+                val thumbnailImage = result.data!!.getParcelableExtra<Bitmap>("data")
+                picView!!.setImageBitmap(thumbnailImage)
             }
         }
     }
