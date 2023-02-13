bc24caeacea65a0d8a714315f98c766a7f9d78c6
Nick <89880711+NMontella438@users.noreply.github.com>
2023-02-12 19:53:21 -0700

    Now the pictures also rotate, the second activity shows the name and picture (also rotates) and there is a restart button

--- app/src/main/java/com/nmontella/app1/DisplayActivity.kt
+++ app/src/main/java/com/nmontella/app1/DisplayActivity.kt
@@ -1,29 +1,50 @@
 package com.nmontella.app1
 
+import android.content.Intent
+import android.graphics.Bitmap
 import android.graphics.BitmapFactory
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.annotation.RequiresApi
+import java.lang.reflect.TypeVariable
 
-class DisplayActivity : AppCompatActivity() {
+class DisplayActivity : AppCompatActivity(), View.OnClickListener {
     private var fullName: String? = null
-
+    private var profilePic: Bitmap? = null
     private var  picView: ImageView? = null
     private var tvFullName: TextView? = null
+    private var restartButton: Button? = null
+    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_display)
         val bundle = intent.extras
-        val picture = bundle!!.getByteArray("image_arr")
-        fullName = bundle.getString("full_name")
+        profilePic = intent.getParcelableExtra("profile_pic", Bitmap::class.java)
+        fullName = bundle!!.getString("full_name")
 
         tvFullName = findViewById(R.id.da_name)
         picView = findViewById(R.id.da_imageView)
 
         tvFullName!!.text = fullName.plus(" ").plus(getString(R.string.login))
-        val bitMap = BitmapFactory.decodeByteArray(picture, 0, picture!!.size)
-        picView!!.setImageBitmap(bitMap)
+        picView!!.setImageBitmap(profilePic)
+
+        restartButton = findViewById(R.id.button_restart)
+        restartButton!!.setOnClickListener(this)
     }
+
+    override fun onSaveInstanceState(outState: Bundle){
+        super.onSaveInstanceState(outState)
+        outState.putString("full_name", fullName)
+    }
+    override fun onClick(p0: View?) {
+        val mainIntent = Intent(this@DisplayActivity, MainActivity::class.java)
+        startActivity(mainIntent)
+    }
+
 }
--- app/src/main/java/com/nmontella/app1/MainActivity.kt
+++ app/src/main/java/com/nmontella/app1/MainActivity.kt
@@ -12,6 +12,7 @@ import android.provider.MediaStore
 import android.view.View
 import android.widget.*
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.RequiresApi
 import java.io.ByteArrayOutputStream
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
@@ -20,6 +21,7 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
     private var firstName: String? = null
     private var midName: String? = null
     private var lastName: String? = null
+    private var profilePic: Bitmap? = null
 
     //UI Variables
     private var buttonSubmit: Button? = null
@@ -29,6 +31,7 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
     private var etMidName: EditText? = null
     private var etLastName: EditText? = null
     private var picView: ImageView? = null
+    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -36,12 +39,14 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
             firstName = savedInstanceState.getString("first_name")
             midName = savedInstanceState.getString("mid_name")
             lastName = savedInstanceState.getString("last_name")
+            profilePic = savedInstanceState.getParcelable("profile_pic", Bitmap::class.java)
         }
         setContentView(R.layout.activity_main)
 
         etFirstName = findViewById(R.id.et_firstName)
         etMidName = findViewById(R.id.et_middleName)
         etLastName = findViewById(R.id.et_lastName)
+        picView = findViewById(R.id.pictureView)
 
         buttonSubmit = findViewById(R.id.button_submit)
         buttonSubmit!!.setOnClickListener(this)
@@ -50,13 +55,24 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         buttonPic = findViewById(R.id.button_picture)
         buttonPic!!.setOnClickListener(this)
 
+        if(profilePic != null)
+            picView!!.setImageBitmap(profilePic)
     }
 
+    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        firstName = savedInstanceState.getString("first_name")
+        midName = savedInstanceState.getString("mid_name")
+        lastName = savedInstanceState.getString("last_name")
+        profilePic = savedInstanceState.getParcelable("profile_pic", Bitmap::class.java)
+    }
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
         outState.putString("first_name", firstName)
         outState.putString("mid_name", midName)
         outState.putString("last_name", lastName)
+        outState.putParcelable("profile_pic", profilePic)
     }
     override fun onClick(view: View) {
         when(view.id){
@@ -83,7 +99,7 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
                     val displayIntent = Intent(this@MainActivity, DisplayActivity::class.java)
                     val bundle = Bundle()
                     bundle.putString("full_name", fullName)
-                    picView
+                    displayIntent.putExtra("profile_pic", profilePic)
                     displayIntent.putExtras(bundle)
                     startActivity(displayIntent)
                 }
@@ -107,9 +123,11 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
             if (Build.VERSION.SDK_INT >= 33) {
                 val thumbnailImage = result.data!!.getParcelableExtra("data", Bitmap::class.java)
                 picView!!.setImageBitmap(thumbnailImage)
+                profilePic = thumbnailImage
             } else {
                 val thumbnailImage = result.data!!.getParcelableExtra<Bitmap>("data")
                 picView!!.setImageBitmap(thumbnailImage)
+                profilePic = thumbnailImage
             }
         }
     }
